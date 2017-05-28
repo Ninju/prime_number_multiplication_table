5de35a11526611aadaaa6c0bd790ba0f4a11c395
Alex <el.zingo@gmail.com>
2017-05-28 13:06:13 +0100

    Add prime number generator

---
+++ lib/prime_number_generator.rb
@@ -0,0 +1,30 @@
+class PrimeNumberGenerator
+  def initialize
+    # important to have 2 & 3 because our algorithm assumes the last prime is
+    # odd.. it also expects there to be at least one prime in the list
+    @primes = [2,3]
+  end
+
+  def take(n)
+    last_prime = primes.last
+
+    if n > last_prime
+      candidate = last_prime + 2
+
+      while primes.size < n
+        unless primes.any? { |prime_factor| (candidate % prime_factor).zero? }
+          primes << candidate
+        end
+
+        # prime can't be even, unless it's '2'
+        candidate += 2
+      end
+    end
+
+    primes.take(n)
+  end
+
+  private
+
+  attr_reader :primes
+end
---
+++ spec/prime_number_generator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe PrimeNumberGenerator do
+  subject { described_class.new }
+
+  it 'gives the first N prime numbers in ascending order' do
+    expect(subject.take(5)).to match_array([2,3,5,7,11])
+  end
+end
